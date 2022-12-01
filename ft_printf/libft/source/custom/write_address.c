25e5c2e9595f1547d8c34918255f34a7afa8da29
Donghyun Kim <donghyun@Donghyuns-MacBook-Pro.local>
2022-12-02 02:31:48 +0900

    1201_3 home

--- ft_printf/libft/source/custom/write_address.c
+++ ft_printf/libft/source/custom/write_address.c
@@ -6,7 +6,7 @@
 /*   By: donghyu2 <donghyu2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 01:29:57 by donghyu2          #+#    #+#             */
-/*   Updated: 2022/12/02 02:11:14 by donghyu2         ###   ########.fr       */
+/*   Updated: 2022/12/02 02:20:21 by donghyu2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ size_t	write_address(unsigned long addr)
 		addr = addr >> 4;
 	}
 	hex[idx] = addr;
+	len += write(1, "0x", 2);
 	while (idx >= 0)
 		len += write_digit(hex[idx--]);
 	return (len);
