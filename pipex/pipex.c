fe517d09245cdd1283c9db19f82ebe5f9bdab8b7
smwkbgmn <6moguai9@gmail.com>
2023-07-21 05:44:05 +0900

    1

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,13 +6,15 @@
 /*   By: donghyu2 <donghyu2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 19:05:17 by donghyu2          #+#    #+#             */
-/*   Updated: 2023/07/21 05:31:13 by donghyu2         ###   ########.fr       */
+/*   Updated: 2023/07/21 05:41:04 by donghyu2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Remove includes in c files */
 /* Remove all comments in c files */
 /* Check about the exit function */
+/* Check the way of parsing parameters for cmds again to
+know whether spliting input by space is the right way or not */
 
 #include "pipex.h"
 
