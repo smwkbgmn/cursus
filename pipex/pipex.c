378e96af461cdbe6fae92cedf49dd05490df4b99
smwkbgmn <6moguai9@gmail.com>
2023-07-21 05:31:20 +0900

    1

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,11 +6,13 @@
 /*   By: donghyu2 <donghyu2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 19:05:17 by donghyu2          #+#    #+#             */
-/*   Updated: 2023/07/21 05:13:29 by donghyu2         ###   ########.fr       */
+/*   Updated: 2023/07/21 05:31:13 by donghyu2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Remove includes in c files */
+/* Remove all comments in c files */
+/* Check about the exit function */
 
 #include "pipex.h"
 
