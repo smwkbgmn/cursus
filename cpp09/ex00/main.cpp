21605bc4f754d4ff34389b730d68efba9b469ee5
smwkbgmn <6moguai9@gmail.com>
2024-03-06 03:32:12 +0900

    1

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -1,12 +1,13 @@
 #include "BitcoinExchange.hpp"
 
-// int main( int argc, char *argv[] )
-int main( void )
+int main( int argc, char *argv[] )
 {
-	try {
-		BitcoinExchange	myData("data.csv");
-		myData.outResult("input.txt");
-	} catch ( err_t &err ) { std::cout << err.what() << std::endl; }
+	if ( argc == 2 ) {
+		try {
+			BitcoinExchange	myData( "data.csv" );
+			myData.outResult( argv[1] );
+		} catch ( err_t &err ) { std::cout << err.what() << std::endl; }
+	}
 
 	return 0;
 }
